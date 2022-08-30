7a3a848352327089342fe1193c9ff1226f83157b
Ronel Andaya <ronel.andaya@gmail.com>
2022-08-31 01:41:00 +0800

    003 First Components & Props

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,37 @@
 <template>
-    <ul>
-        <li v-for="resource in storedResources" :key="resource.id">{{ resource.title }}</li>
-    </ul>
+  <ul>
+    <learning-resource
+      v-for="resource in storedResources"
+      :key="resource.id"
+      :title="resource.title"
+      :description="resource.description"
+      :link="resource.link"
+    ></learning-resource>
+  </ul>
 </template>
 <script>
-    export default {
-        data(){
-            return {
-                storedResources:[
-                    {
-                        id: 'official-guide',
-                        title: 'Official Guide',
-                        description: 'Official vue guide docs',
-                        link: 'https://vuejs.org'
-                    },
-                    {
-                        id: 'google',
-                        title: 'Google',
-                        description: 'search everything',
-                        link: 'https://www.google.com'
-                    }
-                ]
-            }
-        }
-    }
+import LearningResource from './components/learning-resources/LearningResource.vue';
+export default {
+  components: {
+    LearningResource,
+  },
+  data() {
+    return {
+      storedResources: [
+        {
+          id: 'official-guide',
+          title: 'Official Guide',
+          description: 'Official vue guide docs',
+          link: 'https://vuejs.org',
+        },
+        {
+          id: 'google',
+          title: 'Google',
+          description: 'search everything',
+          link: 'https://www.google.com',
+        },
+      ],
+    };
+  },
+};
 </script>
