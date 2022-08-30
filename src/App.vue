dfe78c6b0bb20b03c6174c3b613443aa0dd95b10
Ronel Andaya <ronel.andaya@gmail.com>
2022-08-31 01:19:04 +0800

    002 Setup & First Steps

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+    <ul>
+        <li v-for="resource in storedResources" :key="resource.id">{{ resource.title }}</li>
+    </ul>
+</template>
+<script>
+    export default {
+        data(){
+            return {
+                storedResources:[
+                    {
+                        id: 'official-guide',
+                        title: 'Official Guide',
+                        description: 'Official vue guide docs',
+                        link: 'https://vuejs.org'
+                    },
+                    {
+                        id: 'google',
+                        title: 'Google',
+                        description: 'search everything',
+                        link: 'https://www.google.com'
+                    }
+                ]
+            }
+        }
+    }
+</script>
--- src/main.js
+++ src/main.js
@@ -1,3 +1,8 @@
 import { createApp } from 'vue';
+import App from './App.vue';
 
-createApp({}).mount('#app');
+const app = createApp(App);
+
+app.mount('#app');
+
+
